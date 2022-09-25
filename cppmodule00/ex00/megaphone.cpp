d1f560ba23484129d02c223ee2400bfb4ab59ec4
emirhan ulker <emr.brk.265@gmail.com>
2022-09-25 17:18:28 +0300

    ex00

---
+++ cppmodule00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <cctype>
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+    char c;
+
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    while (av[++i])
+    {
+        for (int j = 0; j < strlen(av[i]); j++)
+        {
+            c = toupper(av[i][j]);
+            std::cout << c;
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
